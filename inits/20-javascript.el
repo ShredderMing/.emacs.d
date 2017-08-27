7e74ac9e197fb79039f1e10e22908703567a8264
ShredderMing <eternallovext@gmail.com>
2017-08-27 21:44:33 +0800

    添加auto-complete-mode。rjsx-mode启用electric-pair-mode

--- inits/20-javascript.el
+++ inits/20-javascript.el
@@ -4,4 +4,7 @@
 (evil-define-key 'insert rjsx-mode-map (kbd "C-d") 'rjsx-delete-creates-full-tag)
 (add-hook 'rjsx-mode-hook
           (lambda ()
-            (setq js-indent-level 2)))
+            (setq js-indent-level 2)
+            (electric-pair-mode t)
+            (auto-complete-mode t);;虽然(global-auto-complete-mode t)，但是没效果。
+            ))
