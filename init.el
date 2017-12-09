09e11b3997b5c5fb6771a530f3f826ee9c936183
ShredderMing <eternallovext@gmail.com>
2017-12-10 01:31:12 +0800

    markdown-mode, remove custom-set-*

--- init.el
+++ init.el
@@ -30,17 +30,3 @@
 (init-loader-load
  (expand-file-name "inits/" *emacs-config-directory*))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (paredit web-mode rjsx-mode recentf-ext prettier-js init-loader helm exec-path-from-shell evil emmet-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:height 120 :family "Menlo")))))
---
+++ inits/20-markdown.el
@@ -0,0 +1,4 @@
+(require-or-install 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
