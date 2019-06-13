8ea6424d538f282deeb181cdf750a9f510ff8ad6
LiuMing <shredderming@gmail.com>
2019-06-14 00:28:16 +0800

    update

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 (setq gc-cons-threshold (* 500 1024 1024))
 
 ;; 加载主配置文件
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+(org-babel-load-file (concat user-emacs-directory "config.org") t)
 
 ;; 还原垃圾回收阀值
 (add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
