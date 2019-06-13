49416988a30b01e497e9a939ddd4be0c78f8123e
LiuMing <shredderming@gmail.com>
2019-06-13 17:39:16 +0800

    tmp

--- init.el
+++ init.el
@@ -1,7 +1,18 @@
-(require 'org)
+;;; init.el --- Global settings -*- lexical-binding: t; -*-
 
-(find-file (concat user-emacs-directory "init.org"))
+;;; Commentary:
 
-(org-babel-tangle)
+;; live with emacs
 
-(load-file (concat user-emacs-directory "init.el"))
+;;; Code:
+
+;; 提高垃圾回收阀值，提高加载速度
+(setq gc-cons-threshold (* 500 1024 1024))
+
+;; 加载主配置文件
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;; 还原垃圾回收阀值
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
+
+;;; init.el ends here
